862b4c428dcd95b2ebed4b8975ded0d98809eabb
T4-HADES <s3870730@rmit.edu.vn>
2022-03-16 23:52:26 +0700

    New Ex4

---
+++ W3/Ex4_Drawing_start.py
@@ -0,0 +1,20 @@
+import turtle
+
+
+def draw_start():
+    win = turtle.Screen()
+
+    tur = turtle.Turtle()
+    tur = turtle.Turtle()
+    tur.pensize(5)
+    tur.speed(8)
+
+    for i in range(5):
+        tur.forward(180)
+        tur.right(144)
+
+    win.exitonclick()
+
+
+if __name__ == '__main__':
+    draw_start()
