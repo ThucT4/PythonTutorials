a25b494554643436192716834b4bea2dde656d90
ThucT4 <94610073+ThucT4@users.noreply.github.com>
2022-03-22 21:15:34 +0700

    Delete W4 directory

--- W4/Ex1_isEven().py
+++
@@ -1,7 +0,0 @@
-def is_even(n):
-    return n % 2 == 0
-
-
-if __name__ == '__main__':
-    print(is_even(8))
-    print(is_even(3))
--- W4/Ex2_isOdd().py
+++
@@ -1,7 +0,0 @@
-def is_odd(n):
-    return n % 2 != 0
-
-
-if __name__ == '__main__':
-    print(is_odd(8))
-    print(is_odd(3))
